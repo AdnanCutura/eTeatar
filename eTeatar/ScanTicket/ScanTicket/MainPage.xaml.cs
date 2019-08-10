1b929a4f6da99d9563fdab9a4247deac79d6f690
AdnanIT <adnan.cutura@gmail.com>
2019-08-11 01:24:46 +0200

    Mobilna aplikacija za skeniranje QR koda i poništavanje karte

---
+++ eTeatar/ScanTicket/ScanTicket/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using DataTransferObjects;
+using DataTransferObjects.Requests;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace ScanTicket
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly APIService _serviceNarudzba = new APIService("Narudzba");
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Skeniraj_kartu(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushAsync(scan);
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    ScanText.Text = "Kod karte: " + result.Text;
+                    await _serviceNarudzba.Update<Narudzba>(result.ToString(), new NarudzbaUpdateRequest
+                    {
+                        IsSkenirana = true
+                    });
+                });
+            };
+        }
+    }
+}
